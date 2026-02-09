d07d0ea7fcec4abe768bed501bcf08dfc252e913
sipicco <piccolo@omrt.tech>
2026-02-09 20:32:06 +0100

    Initial commit. Can find method signature

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using HelloWord;
+using System.Drawing;
+using System.Numerics;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("What to search?");
+        string searchWord = Console.ReadLine();
+
+        var text = "public static bool RectangleContains(Rectangle rect, Vector2 point) " +
+            "{return point.X >= rect.X && point.X <= rect.X + rect.Width && " +
+            "point.Y >= rect.Y && point.Y <= rect.Y + rect.Height; }";
+
+        var service = new searchService();
+
+        var result = service.FindWord(text, searchWord);
+        Console.WriteLine("Found this:");
+        foreach (var word in result)
+        {
+            Console.WriteLine($"{word}");
+        }
+
+
+        // RC -> RectangleContains method
+        // rc -> rect
+    }
+
+    public static bool RectangleContains(Rectangle rect, Vector2 point)
+    {
+        return point.X >= rect.X && point.X <= rect.X + rect.Width &&
+            point.Y >= rect.Y && point.Y <= rect.Y + rect.Height;
+    }
+
+}
---
+++ searchService.cs
@@ -0,0 +1,85 @@
+﻿
+namespace HelloWord
+{
+    internal class searchService
+    {
+        internal List<string> foundWords = new();
+
+
+        string myText = "public static bool RectangleContains(Rectangle rect, Vector2 point) " +
+            "{return point.X >= rect.X && point.X <= rect.X + rect.Width && " +
+            "point.Y >= rect.Y && point.Y <= rect.Y + rect.Height; }";
+
+        // RC -> RectangleContains method
+        // rc -> rect
+
+
+        internal List<string> FindWord(string text, string searchWord)
+        {
+            // is searchWord capital?
+            bool capitalSearch = text.ToCharArray().Any(c => !char.IsUpper(c));
+
+            // capitalSearch -> recognize methods
+
+            var parts = text.Split(" ");
+            int i = 0;
+
+            while (foundWords.Count < 3 && i < parts.Count())
+            {
+                var part = parts[i];
+
+                if (capitalSearch && !IsMethod(part))
+                {
+                    i++;
+                    continue;
+                }
+                if (searchWord.All(c => part.Contains(c)))
+                {
+                    var fullSignature = part;
+
+                    // expand left
+                    for (int j = 1; j < 20; j++)
+                    {
+                        fullSignature = $"{parts[i - j]} " + fullSignature;
+
+                        var test = parts[i - j];
+
+                        if (IsAccessModifier(parts[i - j]))
+                        {
+                            break;
+                        }
+                    }
+                    // expand right
+                    for (int j = 1; j < 20; j++)
+                    {
+                        fullSignature = fullSignature + $" {parts[i + j]}";
+
+                        var test = parts[i + j];
+
+                        if (parts[i + j].Contains(")"))
+                        {
+                            break;
+                        }
+                    }
+
+
+                    foundWords.Add(fullSignature);
+                }
+                i++;
+            }
+            return foundWords;
+        }
+
+        private bool IsAccessModifier(string s)
+        {
+            return s == "public" || s == "private" || s == "internal";
+        }
+
+        private bool IsMethod(string text)
+        {
+            bool startsCapital = char.IsUpper(text[0]);
+            bool hasParenthesis = text.Contains("(");
+            return startsCapital && hasParenthesis;
+        }
+    }
+}
